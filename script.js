ba7b6c3e339bbee2e4408e70e80479aa72a858df
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2024-03-02 12:48:50 +0700

    update navbar and footer

--- script.js
+++ script.js
@@ -34,3 +34,42 @@ function showSlides() {
   // Change image every 3 seconds
   setTimeout(showSlides, 3000);
 }
+
+window.addEventListener("scroll", function () {
+  var navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    // Adjust 50 to the desired scroll position when the change should happen
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  var navbar1 = document.getElementById("navbar-navigation1");
+  var navbar2 = document.getElementById("navbar-navigation2");
+  var navbar3 = document.getElementById("navbar-navigation3");
+  if (window.scrollY > 50) {
+    // Adjust 50 to the desired scroll position when the change should happen
+    navbar1.classList.add("navbar-navigation-scrolled");
+    navbar2.classList.add("navbar-navigation-scrolled");
+    navbar3.classList.add("navbar-navigation-scrolled");
+  } else {
+    navbar1.classList.remove("navbar-navigation-scrolled");
+    navbar2.classList.remove("navbar-navigation-scrolled");
+    navbar3.classList.remove("navbar-navigation-scrolled");
+  }
+});
+window.addEventListener("scroll", function () {
+  console.log(window.scrollY);
+});
+
+window.addEventListener("scroll", function () {
+  var navbar = document.getElementById("hidden-wa");
+  if (window.scrollY > 4160) {
+    // Adjust 50 to the desired scroll position when the change should happen
+    navbar.classList.add("hidden-wa");
+  } else {
+    navbar.classList.remove("hidden-wa");
+  }
+});
